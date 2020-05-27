f5732f8f614ee773a2fab7452f358d6ff7293209
alebaquedano <alebaquedano12@gmail.com>
2020-05-27 17:52:25 -0500

    Actualizacion FINAL Proyecto Final

--- src/test/java/com/fca/calidad/funcionales/PruebasProyectoFinal.java
+++ src/test/java/com/fca/calidad/funcionales/PruebasProyectoFinal.java
@@ -7,121 +7,115 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class PruebasProyectoFinal {
-private WebDriver driver;
-private String baseUrl;
-private boolean acceptNextAlert = true;
-private StringBuffer verificationErrors = new StringBuffer();
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-	@Before
-	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\DELL\\Documents\\calidadDeSoftware\\chromedriver.exe");
-		driver = new ChromeDriver();
-	    baseUrl = "https://mern-crud.herokuapp.com/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
+  @Before
+  public void setUp() throws Exception {
+	  System.setProperty("webdriver.chrome.driver","/Users/Admin2/Documents/ChromeDriver/chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://mern-crud.herokuapp.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-	//ADD
-	//===================================================
-	  @Test
-	  public void testUntitledTestCaseAdd() throws Exception {
-	    driver.get("https://mern-crud.herokuapp.com/");
-	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
-	    driver.findElement(By.name("name")).click();
-	    driver.findElement(By.name("name")).clear();
-	    driver.findElement(By.name("name")).sendKeys("prueba");
-	    driver.findElement(By.name("email")).clear();
-	    driver.findElement(By.name("email")).sendKeys("prueba@gmail.com");
-	    driver.findElement(By.name("age")).click();
-	    driver.findElement(By.name("age")).clear();
-	    driver.findElement(By.name("age")).sendKeys("18");
-	    driver.findElement(By.xpath("//div[3]/div[2]/div/div")).click();
-	    driver.findElement(By.xpath("//div/span")).click();
-	    driver.findElement(By.xpath("//form/button")).click();
-	    driver.findElement(By.xpath("//i")).click();
-	    // Warning: assertTextPresent may require manual changes
-	    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
-	  }
+  @Test
+  //ADD
+  public void testCaseAdd() throws Exception {
+    driver.get("https://mern-crud.herokuapp.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("prueba");
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("prueba@gmail.com");
+    driver.findElement(By.name("age")).click();
+    driver.findElement(By.name("age")).clear();
+    driver.findElement(By.name("age")).sendKeys("18");
+    driver.findElement(By.xpath("//div[3]/div[2]/div/div")).click();
+    driver.findElement(By.xpath("//div/span")).click();
+    driver.findElement(By.xpath("//form/button")).click();
+    driver.findElement(By.xpath("//i")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]prueba@gmail\\.com[\\s\\S]$"));
+  }
+  
+  //EDIT
+  @Test
+  public void testCaseEdit() throws Exception {
+    driver.get("https://mern-crud.herokuapp.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Prueba2");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("prueba2@gmail.com");
+    driver.findElement(By.xpath("//form/button")).click();
+    driver.findElement(By.xpath("//i")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]prueba2[\\s\\S]$"));
+  }
+  
+  //DELETE
+  @Test
+  public void testCaseDelete() throws Exception {
+  driver.get("https://mern-crud.herokuapp.com/");
+  driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+  driver.findElement(By.xpath("//div[3]/button")).click();
+  driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+  driver.findElement(By.xpath("//i")).click();
+  driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+  driver.findElement(By.xpath("//i")).click();
+  driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+  driver.findElement(By.xpath("//i")).click();
+   //Warning: assertTextNotPresent may require manual changes
+  assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]prueba2@gmail\\.com[\\s\\S]$"));
+  }
+  
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
 
-	//EDIT
-	//======================================================
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
 
-	@Test
-	  public void testUntitledTestCaseEdit() throws Exception {
-	    driver.get("https://mern-crud.herokuapp.com/");
-	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
-	    driver.findElement(By.name("name")).click();
-	    driver.findElement(By.name("name")).clear();
-	    driver.findElement(By.name("name")).sendKeys("Prueba2");
-	    driver.findElement(By.name("email")).click();
-	    driver.findElement(By.name("email")).clear();
-	    driver.findElement(By.name("email")).sendKeys("prueba2@gmail.com");
-	    driver.findElement(By.xpath("//form/button")).click();
-	    driver.findElement(By.xpath("//i")).click();
-	    // Warning: assertTextPresent may require manual changes
-	    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
-	  }
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
 
-	//DELETE
-	//=======================================================
-
-	 @Test
-	  public void testUntitledTestCaseDelete() throws Exception {
-	    driver.get("https://mern-crud.herokuapp.com/");
-	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
-	    driver.findElement(By.xpath("//div[3]/button")).click();
-	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
-	    driver.findElement(By.xpath("//i")).click();
-	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
-	    driver.findElement(By.xpath("//i")).click();
-	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
-	    driver.findElement(By.xpath("//i")).click();
-	    // Warning: assertTextNotPresent may require manual changes
-	    assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]prueba2@gmail\\.com[\\s\\S]$"));
-	  }
-
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
-
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
-
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
-
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
-	}
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
